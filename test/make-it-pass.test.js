b8c1c3b8afb4bb3b73c4e38eb1b62903b1ae873d
idandvd <idandvd@users.noreply.github.com>
2018-06-10 20:30:02 +0300

    GREEN

--- test/make-it-pass.test.js
+++ test/make-it-pass.test.js
@@ -2,10 +2,13 @@ const test = require('tape'); // assign the tape library to the variable "test"
 
 function sum (a, b) {
   // your code to make the test pass goes here ...
-  return a + b;
+  return a + b+a;
 }
-
+function mult(a, b) {
+  // your code to make the test pass goes here ...
+  return a * b;
+}
 test('sum should return the addition of two numbers', function (t) {
-  t.equal(3, sum(1, 2)); // make this test pass by completing the add function!
+  t.equal(2, sum(1, 2)); // make this test pass by completing the add function!
   t.end();
 });
